f9b8d65b1d3a568ac08e17209caa223e7c6022de
Martin Sand <martin.sand.dev@gmail.com>
2021-06-04 11:04:29 +0200

    Added basic cmake support
    Signed-off-by: Martin Sand <martin.sand.dev@gmail.com>

--- example/example.cpp
+++ example/example.cpp
@@ -44,10 +44,10 @@ void print_depthCache() {
 	map < string, map <double,double> >::iterator it_i;
 
 	for ( it_i = depthCache.begin() ; it_i != depthCache.end() ; it_i++ ) {
-			
+
 		string bid_or_ask = (*it_i).first ;
 		cout << bid_or_ask << endl ;
-		cout << "Price             Qty" << endl ;		
+		cout << "Price             Qty" << endl ;
 
 		map <double,double>::reverse_iterator it_j;
 
@@ -70,7 +70,7 @@ void print_klinesCache() {
 	map < long, map<string,double> >::iterator it_i;
 
 	cout << "==================================" << endl;
-	
+
 	for ( it_i = klinesCache.begin() ; it_i != klinesCache.end() ; it_i++ ) {
 
 		long start_of_candle 		= (*it_i).first;
@@ -94,7 +94,7 @@ void print_aggTradeCache() {
 	map < long, map<string,double> >::iterator it_i;
 
 	cout << "==================================" << endl;
-	
+
 	for ( it_i = aggTradeCache.begin() ; it_i != aggTradeCache.end() ; it_i++ ) {
 
 		long timestamp 			= (*it_i).first;
@@ -118,7 +118,7 @@ void print_userBalance() {
 	map < string, map<string,double> >::iterator it_i;
 
 	cout << "==================================" << endl;
-	
+
 	for ( it_i = userBalance.begin() ; it_i != userBalance.end() ; it_i++ ) {
 
 		string symbol 			= (*it_i).first;
@@ -142,13 +142,13 @@ void print_userBalance() {
 
 //-------------
 int ws_depth_onData( Json::Value &json_result ) {
-	
+
 	int i;
 
 	int new_updateId  = json_result["u"].asInt();
-	
+
 	if ( new_updateId > lastUpdateId ) {
-	
+
 		for ( i = 0 ; i < json_result["b"].size() ; i++ ) {
 			double price = atof( json_result["b"][i][0].asString().c_str());
 			double qty 	 = atof( json_result["b"][i][1].asString().c_str());
@@ -166,7 +166,7 @@ int ws_depth_onData( Json::Value &json_result ) {
 			} else {
 				depthCache["asks"][price] = qty;
 			}
-		}		
+		}
 		lastUpdateId = new_updateId;
 	}
 	print_depthCache();
@@ -179,15 +179,15 @@ int ws_depth_onData( Json::Value &json_result ) {
 
 //-------------
 int ws_klines_onData( Json::Value &json_result ) {
-	
+
 	long start_of_candle = json_result["k"]["t"].asInt64();
 	klinesCache[start_of_candle]["o"] = atof( json_result["k"]["o"].asString().c_str() );
 	klinesCache[start_of_candle]["h"] = atof( json_result["k"]["h"].asString().c_str() );
 	klinesCache[start_of_candle]["l"] = atof( json_result["k"]["l"].asString().c_str() );
 	klinesCache[start_of_candle]["c"] = atof( json_result["k"]["c"].asString().c_str() );
 	klinesCache[start_of_candle]["v"] = atof( json_result["k"]["v"].asString().c_str() );
-	
-	print_klinesCache();	
+
+	print_klinesCache();
 }
 
 
@@ -203,7 +203,7 @@ int ws_aggTrade_OnData( Json::Value &json_result ) {
 	long timestamp = json_result["T"].asInt64();
 	aggTradeCache[timestamp]["p"] = atof( json_result["p"].asString().c_str() );
 	aggTradeCache[timestamp]["q"] = atof( json_result["q"].asString().c_str() );
-		
+
 	print_aggTradeCache();
 }
 
@@ -219,28 +219,28 @@ int ws_userStream_OnData( Json::Value &json_result ) {
 		string executionType = json_result["x"].asString();
 		string orderStatus   = json_result["X"].asString();
 		string reason        = json_result["r"].asString();
-		string symbol 	     = json_result["s"].asString(); 	
-		string side 	     = json_result["S"].asString();	
-		string orderType     = json_result["o"].asString();	
-		string orderId 	     = json_result["i"].asString();	
+		string symbol 	     = json_result["s"].asString();
+		string side 	     = json_result["S"].asString();
+		string orderType     = json_result["o"].asString();
+		string orderId 	     = json_result["i"].asString();
 		string price 	     = json_result["p"].asString();
 		string qty 	     = json_result["q"].asString();
 
 		if ( executionType  == "NEW" ) {
 			if ( orderStatus == "REJECTED" ) {
-				printf(KRED"Order Failed! Reason: %s\n"RESET, reason.c_str() );
+				printf( KRED "Order Failed! Reason: %s\n" RESET, reason.c_str() );
 			}
-			printf(KGRN"\n\n%s %s %s %s(%s) %s %s\n\n"RESET, symbol.c_str() , side.c_str() , orderType.c_str() , orderId.c_str() , orderStatus.c_str(), price.c_str(), qty.c_str() );
+			printf( KGRN "\n\n%s %s %s %s(%s) %s %s\n\n" RESET, symbol.c_str() , side.c_str() , orderType.c_str() , orderId.c_str() , orderStatus.c_str(), price.c_str(), qty.c_str() );
 			return 0;
 		}
-		printf(KBLU"\n\n%s %s %s %s %s\n\n"RESET, symbol.c_str() , side.c_str() , executionType.c_str() , orderType.c_str() , orderId.c_str() );
+		printf( KBLU " \n\n%s %s %s %s %s\n\n" RESET, symbol.c_str() , side.c_str() , executionType.c_str() , orderType.c_str() , orderId.c_str() );
 
 
 	} else if ( action == "outboundAccountInfo" ) {
 
 		// Update user balance
 		for ( i = 0 ; i < json_result["B"].size() ; i++ ) {
-			
+
 			string symbol = json_result["B"][i]["a"].asString();
 			userBalance[symbol]["f"] = atof( json_result["B"][i]["f"].asString().c_str() );
 			userBalance[symbol]["l"] = atof( json_result["B"][i]["f"].asString().c_str() );
@@ -256,8 +256,8 @@ int ws_userStream_OnData( Json::Value &json_result ) {
 //---------------------------
 /*
 	Examples of how to use BinaCPP Binance API library
-	Simply uncomment out the code and compile with : 
-	
+	Simply uncomment out the code and compile with :
+
 	make example
 
 	and run with
@@ -276,13 +276,13 @@ int main() {
 
 
 	/*
-		The Json::value object each element can be access like hash map <>, 
+		The Json::value object each element can be access like hash map <>,
 		or vector <> if it is Json::array
 	*/
 	Json::Value result;
-	long recvWindow = 10000;	
+	long recvWindow = 10000;
 
-	
+
 	//------------------------------------
 	// Example : Get Server Time.
 	BinaCPP::get_serverTime( result ) ;
@@ -305,7 +305,7 @@ int main() {
 
 	/*
 	// -------------------------------------------------------------
-	// Example: Get Account 
+	// Example: Get Account
 	BinaCPP::get_account( recvWindow , result );
 	cout << result << endl;
 	//*/
@@ -327,14 +327,14 @@ int main() {
 	*/
 
 
-	/*-------------------------------------------------------------		
+	/*-------------------------------------------------------------
 	// Example: Get Depth of single pair
 	BinaCPP::get_depth( "ETHBTC", 5, result ) ;
 	cout << result << endl;
 	*/
 
 
-	
+
 	//-------------------------------------------------------------
 	// Example: Placing a LIMIT order
 	//BinaCPP::send_order( "BNBETH", "BUY", "LIMIT", "GTC", 20 , 0.00380000, "",0,0, recvWindow, result );
@@ -357,13 +357,13 @@ int main() {
 	/*-------------------------------------------------------------
 	// Example: Check an order's status
 	BinaCPP::get_order( "BNBETH", 12345678, "", recvWindow, result );
-	cout << result << endl;	 
+	cout << result << endl;
 	*/
 
-	/*-------------------------------------------------------------	
+	/*-------------------------------------------------------------
 	// Example: Cancel an order
 	BinaCPP::cancel_order("BNBETH", 12345678, "","", recvWindow, result);
-	cout << result << endl;	 
+	cout << result << endl;
 	*/
 
 
@@ -378,7 +378,7 @@ int main() {
 
 	/*-------------------------------------------------------------
 	// Example: Get all account orders; active, canceled, or filled.
-	BinaCPP::get_allOrders( "BNBETH", 0,0, recvWindow, result ) 
+	BinaCPP::get_allOrders( "BNBETH", 0,0, recvWindow, result )
 	cout << result << endl;
 	*/
 
@@ -387,19 +387,19 @@ int main() {
 	// Example : Get all trades history
 	/*
 	BinaCPP::get_myTrades( "BNBETH", 0,0, recvWindow , result );
-	cout << result << endl;	 
+	cout << result << endl;
 	*/
 
-		
+
 	/*-------------------------------------------------------------
 	/*
 	// Example: Getting 24hr ticker price change statistics for a symbol
 	   BinaCPP::get_24hr( "ETHBTC", result ) ;
 	   cout << result << endl;
-	*/   
+	*/
 
-	
-	/*-------------------------------------------------------------   
+
+	/*-------------------------------------------------------------
 	/*
 	// Example: Get Kline/candlestick data for a symbol
 	  BinaCPP::get_klines( "ETHBTC", "1h", 10 , 0, 0, result );
@@ -407,24 +407,24 @@ int main() {
 	*/
 
 
-	/*-------------------------------------------------------------  
+	/*-------------------------------------------------------------
 	/* Websockets Endpoints */
 
-	
-	 
 
 
 
 
-		
-	// Market Depth 
+
+
+
+	// Market Depth
 	int i;
 	string symbol = "BNBBTC";
 	BinaCPP::get_depth( symbol.c_str(), 20, result ) ;
 
 	// Initialize the lastUpdateId
 	lastUpdateId = result["lastUpdateId"].asInt();
- 		
+
 	for ( int i = 0 ; i < result["asks"].size(); i++ ) {
 
 		double price = atof( result["asks"][i][0].asString().c_str() );
@@ -438,14 +438,14 @@ int main() {
 		depthCache["bids"][price] = qty;
 	}
 	print_depthCache();
- 	
 
 
 
-	
- 	
 
- 	
+
+
+
+
  	// Klines/CandleStick
  	BinaCPP::get_klines( "ETHBTC", "1h", 10 , 0, 0, result );
  	for ( int i = 0 ; i < result.size() ; i++ ) {
@@ -457,13 +457,13 @@ int main() {
  		klinesCache[start_of_candle]["v"] = atof( result[i][5].asString().c_str() );
  	}
  	print_klinesCache();
- 	
-
- 	
 
 
-	//  AggTrades 
-	BinaCPP::get_aggTrades( "BNBBTC", 0, 0, 0, 10, result ); 
+
+
+
+	//  AggTrades
+	BinaCPP::get_aggTrades( "BNBBTC", 0, 0, 0, 10, result );
 	for ( int i = 0 ; i < result.size() ; i++ ) {
 		long timestamp = result[i]["T"].asInt64();
  		aggTradeCache[timestamp]["p"] =  atof( result[i]["p"].asString().c_str() );
@@ -471,8 +471,8 @@ int main() {
  	}
 	print_aggTradeCache();
 
-	
-	
+
+
 
 
 	// User Balance
@@ -489,27 +489,27 @@ int main() {
 
 
 
-	// User data stream	
+	// User data stream
 	BinaCPP::start_userDataStream(result );
-	
+
 	cout << result << endl;
-	
+
 	string ws_path = string("/ws/");
 	ws_path.append( result["listenKey"].asString() );
 
 
-	
-	
+
+
 	BinaCPP_websocket::init();
- 	
- 	BinaCPP_websocket::connect_endpoint( ws_aggTrade_OnData ,"/ws/bnbbtc@aggTrade" ); 
-	BinaCPP_websocket::connect_endpoint( ws_userStream_OnData , ws_path.c_str() ); 
-	BinaCPP_websocket::connect_endpoint( ws_klines_onData ,"/ws/bnbbtc@kline_1m" ); 
-	BinaCPP_websocket::connect_endpoint( ws_depth_onData ,"/ws/bnbbtc@depth" ); 
- 		
-	BinaCPP_websocket::enter_event_loop(); 
-	
-	
+
+ 	BinaCPP_websocket::connect_endpoint( ws_aggTrade_OnData ,"/ws/bnbbtc@aggTrade" );
+	BinaCPP_websocket::connect_endpoint( ws_userStream_OnData , ws_path.c_str() );
+	BinaCPP_websocket::connect_endpoint( ws_klines_onData ,"/ws/bnbbtc@kline_1m" );
+	BinaCPP_websocket::connect_endpoint( ws_depth_onData ,"/ws/bnbbtc@depth" );
+
+	BinaCPP_websocket::enter_event_loop();
+
+
 
 	return 0;
 }
